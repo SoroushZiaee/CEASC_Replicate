1d4c600b3fce5bb26d3723343938a54d5d4a10fb
Soroush Ziaee <soroushziaee1@gmail.com>
2025-03-25 10:09:05 -0400

    - add bash setting to our current directory

---
+++ bash/setup_notebook/notebook_on_cpu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=jupyter
+#SBATCH --output=jupyter_cpu.out
+#SBATCH --error=jupyter_cpu.err
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=12:00:00
+#SBATCH --mem=20G
+#SBATCH --mail-type=BEGIN,END,FAIL # Send email on job END and FAIL
+#SBATCH --mail-user=soroush1@yorku.ca
+
+echo "Start Installing and setup env"
+source bash/setup_notebook/setup_env_node.sh
+
+module list
+
+pip freeze
+
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+
+pip install --no-index --upgrade pip
+
+echo "Installing requirements"
+pip install --no-index -r requirements.txt
+
+echo "Env has been set up"
+
+pip freeze
+
+bash/setup_notebook/lab.sh
