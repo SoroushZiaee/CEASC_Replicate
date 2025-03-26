216272e22893131ad940244e577f8c2b82722593
Elizaveta Yakubovskaya <eyakub@my.yorku.ca>
2025-03-26 10:00:42 -0400

    updates to amm.py loss and conv kernel

--- models/modules/amm.py
+++ models/modules/amm.py
@@ -7,7 +7,7 @@ import numpy as np
 class AMM_module(torch.nn.Module):
     def __init__(self, n_classes):
         super(AMM_module, self, n_classes). __init__()
-        self.conv = torch.nn.Conv2d(3,n_classes,3,padding='same') # assuming input has 3 channels (e.g., RGB) and there are 10 classes - we need one binary mask per class 
+        self.conv = torch.nn.Conv2d(3,1,3,padding='same') # assuming input has 3 channels (e.g., RGB) and set number of output channels to 1 based on paper specs
     def forward(self,x):
         c1 = self.conv(x)
         s1 = c1 + torch.nn.functional.gumbel_softmax(c1,dim=1) # make sure this implimentation is the same as the paper and contains learnable parameters as discussed in the paper 
@@ -23,8 +23,8 @@ class AMM_loss(torch.nn.Module):
     def forward(self,h,label):
         l = [] # will contain the loss for each layer 
         for i in range(len(label)): # for the ground truth mask of each layer of the FPN
-            pi = sum(torch.argwhere(label[i]>0))/(label[i].shape[1]*label[i].shape[2]*label[i].shape[3]) # ratio of pixels containing classified objects to total pixels in GT - assumes batch size of one currently
-            li = ((sum(torch.argwhere(h[i]>0))/(h[i].shape[1]*h[i].shape[2]*h[i].shape[3]))-pi)**2 # difference in ratio between the label ratio and data ratio
+            pi = sum(torch.argwhere(label[i]>0))/(label[i].size[1]*label[i].size[2]*label[i].size[3]) # ratio of pixels containing classified objects to total pixels in GT - assumes batch size of one currently
+            li = ((sum(torch.argwhere(h[i]>0))/(h[i].size[1]*h[i].size[2]*h[i].size[3]))-pi)**2 # difference in ratio between the label ratio and data ratio
             l.append(li)
         l_amm = sum(l)/len(l)
         return l_amm
