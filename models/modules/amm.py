587ddeedc2175570d36a2f39fccf16b74418cd53
Elizaveta Yakubovskaya <eyakub@my.yorku.ca>
2025-03-26 22:56:13 -0400

    fixed gumbel

--- models/modules/amm.py
+++ models/modules/amm.py
@@ -9,19 +9,23 @@ class AMM_module(torch.nn.Module):
         super(AMM_module, self). __init__()
         self.modes = ["train","test"]
         self.conv = torch.nn.Conv2d(256,1,3,padding='same') # number of input channels is 256 based on FPN feature channel dimensions and output is one channel based on paper 
+        self.temp = 1 # temperature on Gumble-Softmax 
     def forward(self,xi,mode):
         if mode not in self.modes:
             raise ValueError(f"Invalid mode. Expected one of: {self.modes}") # make sure training or testing mode for amm is set
         si = self.conv(xi)
-        hi = torch.empty_like(si,requires_grad=True) # baseline for the binary mask -- set requires grad as true for now, might not be the case later -- work through this 
         if mode == "train":
-
+            g1 = torch.from_numpy(np.random.gumbel(size=si.size())) # make random Gumbel noise
+            g2 = torch.from_numpy(np.random.gumbel(size=si.size()))
+            hi = torch.nn.Sigmoid((si+g1-g2)/self.temp) # modulate si by the noise and push through the sigmoid function 
+            hard = hi >= 0.5 # make a binary mask from soft values -- hard thresholding 
+            hi[hard] = 1
+            hi[~hard] = 0
         else:
-            hard = torch.argwhere(si > 0)
+            hi = torch.empty_like(si) # baseline for the binary mask 
+            hard = si > 0
             hi[hard] = 1 # set all the values where si > 0 to 1
             hi[~hard] = 0 # set all the values where si < 0 to 0
-
-        s1 = c1 + torch.nn.functional.gumbel_softmax(c1,dim=1) # make sure this implimentation is the same as the paper and contains learnable parameters as discussed in the paper 
         return hi
 
 class AMM_loss(torch.nn.Module):
@@ -30,8 +34,8 @@ class AMM_loss(torch.nn.Module):
     def forward(self,h,label):
         l = [] # will contain the loss for each layer 
         for i in range(len(label)): # for the ground truth mask of each layer of the FPN
-            pi = sum(torch.argwhere(label[i]>0))/(label[i].size[1]*label[i].size[2]*label[i].size[3]) # ratio of pixels containing classified objects to total pixels in GT - assumes batch size of one currently
-            li = ((sum(torch.argwhere(h[i]>0))/(h[i].size[1]*h[i].size[2]*h[i].size[3]))-pi)**2 # difference in ratio between the label ratio and data ratio
+            pi = sum(label[i]>0)/(label[i].size[2]*label[i].size[3]) # ratio of pixels containing classified objects to total pixels in GT - currently assumes batch size of 1
+            li = ((sum(h[i]>0)/(h[i].size[2]*h[i].size[3]))-pi)**2 # difference in ratio between the label ratio and data ratio
             l.append(li)
         l_amm = sum(l)/len(l)
         return l_amm
