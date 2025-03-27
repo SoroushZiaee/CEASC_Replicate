a2d3eb6ac2bf5c0a62658d3447537ec27df917de
Soroush Ziaee <soroushziaee1@gmail.com>
2025-03-27 08:43:13 -0400

    - update test function

--- test/test_end_to_end_module.py
+++ test/test_end_to_end_module.py
@@ -20,18 +20,33 @@ def test_ceasc_model(config_path, num_classes=10, batch_size=2, image_size=512):
 
     # Forward pass
     with torch.no_grad():
-        cls_outs, reg_outs, cls_softs, reg_softs = model(x, stage="val")
+        (
+            cls_outs,
+            reg_outs,
+            cls_softs,
+            reg_softs,
+            sparse_cls_feats,
+            sparse_reg_feats,
+            dense_cls_feats,
+            dense_reg_feats,
+            fpn_feats,
+        ) = model(x, stage="val")
 
     # Print output shape info
-    print("Test CEASC Model Output Shapes:")
+    print("🧪 Test CEASC Model Output Shapes:")
     for i, (cls, reg, c_mask, r_mask) in enumerate(
         zip(cls_outs, reg_outs, cls_softs, reg_softs)
     ):
         print(f"FPN Level P{i+3}:")
-        print(f"\t\t- cls_out:       {cls.shape}")  # [B, num_classes, H, W]
-        print(f"\t\t- reg_out:       {reg.shape}")  # [B, 4, H, W]
-        print(f"\t\t- cls_soft_mask: {c_mask.shape}")  # [B, 1, H, W]
-        print(f"\t\t- reg_soft_mask: {r_mask.shape}")  # [B, 1, H, W]")
+        print(f"\t- cls_out:         {cls.shape}")  # [B, num_classes, H, W]
+        print(f"\t- reg_out:         {reg.shape}")  # [B, 4, H, W]
+        print(f"\t- cls_soft_mask:   {c_mask.shape}")  # [B, 1, H, W]
+        print(f"\t- reg_soft_mask:   {r_mask.shape}")  # [B, 1, H, W]")
+
+        print(f"\t- sparse_cls_feat[0]: {sparse_cls_feats[i][0].shape}")  # [B, C, H, W]
+        print(
+            f"\t- dense_cls_feat[0]:  {dense_cls_feats[i][0].shape}"
+        )  # [B, C, H, W]")
 
 
 if __name__ == "__main__":
