36056391a0c89ff15f9ba9bad5e44a5172ea9faf
Soroush Ziaee <soroushziaee1@gmail.com>
2025-03-27 07:46:50 -0400

    - build our end-to-end module

---
+++ models/resnet18_fpn_ceasc_network.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+
+from models.backbone.gfl_v1_fpn import ResNet18FPN
+from models.modules.ceasc import CEASC
+
+
+class Res18FPNCeasc(nn.Module):
+    def __init__(self, config_path: str, num_classes: int = 10):
+        super(Res18FPNCeasc, self).__init__()
+
+        self.backbone = ResNet18FPN(config_path=config_path, pretrained=True)
+        self.detection_heads = nn.ModuleList(
+            [CEASC(256, 256, num_classes=num_classes) for _ in range(5)]
+        )
+
+    def forward(self, x, stage: str = "train"):
+        feats = self.backbone(x)
+        cls_outs = []
+        reg_outs = []
+        cls_soft_mask_outs = []
+        reg_soft_mask_outs = []
+
+        for head, feat in zip(self.detection_heads, feats):
+            cls_out, reg_out, cls_soft_mask, reg_soft_mask = head(feat, stage)
+            cls_outs.append(cls_out)
+            reg_outs.append(reg_out)
+            cls_soft_mask_outs.append(cls_soft_mask)
+            reg_soft_mask_outs.append(reg_soft_mask)
+
+        return cls_outs, reg_outs, cls_soft_mask_outs, reg_soft_mask_outs
