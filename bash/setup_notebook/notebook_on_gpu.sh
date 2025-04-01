6c58fbafbef42790540a8e370b826f67c742e7d3
Soroush Ziaee <soroushziaee1@gmail.com>
2025-03-31 21:53:25 -0400

    - submit gpu worker

---
+++ bash/setup_notebook/notebook_on_gpu.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=jupyter
+#SBATCH --output=jupyter_gpu.out
+#SBATCH --error=jupyter_gpu.err
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:a100:1
+#SBATCH --time=12:00:00
+#SBATCH --mem=20G
+#SBATCH --mail-type=BEGIN,END,FAIL # Send email on job END and FAIL
+#SBATCH --mail-user=soroush1@yorku.ca
+
+echo "Start Installing and setup env"
+source bash/setup_notebook/setup_env_node.sh
+
+module list
+
+pip freeze
+
+# virtualenv --no-download $SLURM_TMPDIR/env
+source /home/soroush1/projects/def-kohitij/soroush1/CEASC_Replicate/.venv/bin/activate
+
+pip install --no-index --upgrade pip
+
+echo "Installing requirements"
+# pip install --no-index -r requirements.txt
+
+echo "Env has been set up"
+
+pip freeze
+
+bash/setup_notebook/lab.sh
