a677a6362c4c646657aa980e381f37e1d15733a7
Elizaveta Yakubovskaya <eyakub@my.yorku.ca>
2025-04-02 10:22:46 -0400

    added classes

--- scripts/metrics/accuracy.py
+++ scripts/metrics/accuracy.py
@@ -1,10 +1,16 @@
 # the accuracy (precision and recall) metrics we are focusing on reproducing
+import torch
 
 # mAP
 
-# AP50 
 
-# AP75 
+# AP50 and AP75 
+class AP(torch.nn.Module):
+    def __init__(self,t):
+        super(AP, self,t).__init__()
+        self.t = t
+    def forward():
+
 
 # AR1
 
