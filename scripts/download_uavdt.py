1ce7c088d73e2355d65e43800cec350a2e9b7529
Elizaveta Yakubovskaya <eyakub@my.yorku.ca>
2025-04-08 12:21:32 -0400

    added new things to download

--- scripts/download_uavdt.py
+++ scripts/download_uavdt.py
@@ -1,5 +1,5 @@
 import os 
-import requests 
+from urllib.request import urlretrieve
 from zipfile import ZipFile 
 
 def main():
@@ -9,21 +9,43 @@ def main():
     user = cwd.split('/')[4]
 
     # make the path where to store the dataset -- ALTER THIS FOR A DIFFERENT SYSTEM
-    dataset_z = f'/home/{user}/scratch/uavdt.zip'
-    dataset_a = f'/home/{user}/scratch'
+    dataset_z = f'/home/{user}/scratch/CEASC_replicate/uavdt.zip'
+    toolkit_z = f'/home/{user}/scratch/CEASC_replicate/uav_toolkit.zip'
+    unzip_path = f'/home/{user}/scratch/CEASC_replicate'
 
     # set the url to UAV benchmark M
-    # url = 'https://drive.google.com/file/d/1m8KA6oPIRK_Iwt9TYFquC87vBc_8wRVc/view'
-    url = "https://docs.google.com/uc?export=download&id=1m8KA6oPIRK_Iwt9TYFquC87vBc_8wRVc"
+    url1 = "https://drive.usercontent.google.com/download?id=1m8KA6oPIRK_Iwt9TYFquC87vBc_8wRVc&export=download&authuser=0&confirm=t&uuid=7a93cd34-815b-4fc7-97b6-22094740cc10&at=APcmpowzWpKboI9qLGzupSiO_NZK%3A1744120578388"
 
-    # download dataset to the specified file path
-    path, __ = urlretrieve(url,dataset_z)
+    # set another url to UAV toolkit 
+    url2 = "https://drive.usercontent.google.com/download?id=19498uJd7T9w4quwnQEy62nibt3uyT9pq&export=download&authuser=0&confirm=t&uuid=c71dfb09-1273-4ded-8e01-3886a225a27b&at=APcmpoyiy4L3fIXfC5i5qpSGcDaN%3A1744123326366"
 
-    print(f'UAVDT downloaded to: {path}')
+    if os.path.exists(f"/home/{user}/scratch/CEASC_replicate/UAV-benchmark-M/") == False:
+        print("Downloading UAVDT")
 
-    # extract all the data from the file
-    with ZipFile(dataset_z,'r') as zobj:
-        zobj.extractall(path=dataset_a)
+        # download dataset to the specified file path
+        path1, __ = urlretrieve(url1,dataset_z)
+
+        print(f'UAVDT downloaded to: {path1}')
+
+        # extract all the data from the file
+        with ZipFile(dataset_z,'r') as zobj1:
+            zobj1.extractall(path=unzip_path)
+    else:
+        print("UAVDT already exists. Skipping download.")
+    
+    if os.path.exists(f"home/{user}/scratch/CEASC_replicate/UAV-benchmark-MOTD_v1.0/") == False:
+        print("Downloading UAVDT Toolkit")
+
+        # download dataset to the specified file path
+        path2, __ = urlretrieve(url2,toolkit_z)
+
+        print(f'UAVDT Toolkit downloaded to: {path2}')
+
+        # extract all the data from the file
+        with ZipFile(toolkit_z,'r') as zobj2:
+            zobj2.extractall(path=unzip_path)
+    else:
+        print("UAVDT Toolkit already exists. Skipping download.")
 
     pass
 
