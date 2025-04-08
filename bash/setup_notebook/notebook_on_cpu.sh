5e370f17dbef13a53e4991787bbe4f56c6f16526
Soroush Ziaee <soroushziaee1@gmail.com>
2025-04-08 09:50:01 -0400

    - change time from 12 to 2 hours for ease of use

--- bash/setup_notebook/notebook_on_cpu.sh
+++ bash/setup_notebook/notebook_on_cpu.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=jupyter_cpu.err
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=10
-#SBATCH --time=12:00:00
+#SBATCH --time=2:00:00
 #SBATCH --mem=20G
 #SBATCH --mail-type=BEGIN,END,FAIL # Send email on job END and FAIL
 #SBATCH --mail-user=soroush1@yorku.ca
