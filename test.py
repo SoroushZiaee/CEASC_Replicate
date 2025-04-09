fae9798f7a8f2c82dd642d0b698b0b2a8b756d28
Elizaveta Yakubovskaya <eyakub@my.yorku.ca>
2025-04-09 15:17:06 -0400

    updates

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
 import os 
-cwd = os.getcwd()
-print(cwd)
-user = cwd.split('/')[4]
-print(user)
+movies = os.listdir('/home/eyakub/scratch/CEASC_replicate/UAV-benchmark-M')
+
+testset = ['M0203', 'M0205', 'M0208', 'M0209', 'M0403', 'M0601', 'M0602', 'M0606',
+            'M0701', 'M0801', 'M0802', 'M1001',
+            'M1004', 'M1007', 'M1009', 'M1101', 'M1301', 'M1302', 'M1303', 'M1401']
+
+set2 = []
+
+trainset = []
+
+for movie in movies:
+    if movie not in testset:
+        trainset.append(movie)
+
+    else:
+        set2.append(movie)
+
+print(trainset)
+
+print(set2)
