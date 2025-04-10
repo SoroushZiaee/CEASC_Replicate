d8304d4a1a429fc43a40c8939a463531f87e932a
Elizaveta Yakubovskaya <eyakub@my.yorku.ca>
2025-04-10 14:47:26 -0400

    adding live annotation generation

--- data/uavdt_dataset.py
+++ data/uavdt_dataset.py
@@ -1,5 +1,7 @@
 from torch.utils.data import Dataset
 from torchvision import transforms
+from PIL import Image
+import numpy as np
 
 import os 
 
@@ -16,16 +18,9 @@ class UAVDTDataset(Dataset):
             2: "bus" 
         }
 
-        if split == "train":
-            self.img_dir = os.path.join(self.root_dir, 'UAV-benchmark-M-train')
-            self.anno_path = os.path.join(self.root_dir, 'UAV-benchmark-M-train', 'train_annos.json') # make sure these paths work
-        
-        else:
-            self.img_dir = os.path.join(self.root_dir, 'UAV-benchmark-M-test')
-            self.anno_path = os.path.join(self.root_dir, 'UAV-benchmark-M-test', 'test_annos.json')
-
-        self.imgs = [f for f in os.listdir(self.img_dir) if f.endswith(".jpg")]
-        self.imgs.sort()
+        with open(os.path.join(self.root_dir, "UAV-benchmark-M-train", f"{split}set_meta.csv"), 'r') as file:
+            self.imgs = file.readlines() # get the list of imgs from the csv file 
+        file.close()
 
         if self.transform is None:
             self.transform = transforms.Compose(
@@ -37,5 +32,35 @@ class UAVDTDataset(Dataset):
     def __len__(self):
         return len(self.imgs)
     
-    def parse_annotation(self, anno_path):
-        # start here once the annotations have been made
+    def parse_annotation(self):
+        boxes = []
+        labels = []
+
+        # Get the annotations from the toolkit gt files 
+
+
+    def __getitem__(self, idx):
+        """
+        Get data sample by index
+
+        Args:
+            idx (int): Index
+
+        Returns:
+            Dictionary containing image and annotation information
+        """
+        img_path = self.imgs[idx].split('\\')[0] # get the path without the \n character
+
+        # Load the image
+        image = Image.open(img_path).convert("RGB")
+        orig_width, orig_height = image.size
+
+        # Load annotations if available - if test set them to 0
+        if self.split == "test":
+            targets = {
+                "boxes": np.zeros((0, 4), dtype=np.float32),
+                "labels": np.zeros((0,), dtype=np.int64),
+            }
+            # figure out if we are also trying to predict other things from this dataset -- look through the paper again 
+        else:
+            targets = self.parse_annotation()
--- test.py
+++ test.py
@@ -1,24 +1,8 @@
-import os 
-movies = os.listdir('/home/eyakub/scratch/CEASC_replicate/UAV-benchmark-M')
-movies.sort()
+import os
 
-testset = ['M0203', 'M0205', 'M0208', 'M0209', 'M0403', 'M0601', 'M0602', 'M0606',
-            'M0701', 'M0801', 'M0802', 'M1001',
-            'M1004', 'M1007', 'M1009', 'M1101', 'M1301', 'M1302', 'M1303', 'M1401']
+root_dir = "/home/eyakub/scratch/CEASC_replicate"
 
-val_size = int(len(testset)*0.1)
+with open(os.path.join(root_dir, "UAV-benchmark-M", f"valset_meta.csv"), 'r') as file:
+    imgs = file.readlines()
 
-set2 = []
-
-trainset = []
-
-for movie in movies:
-    if movie not in testset:
-        trainset.append(movie)
-
-    else:
-        set2.append(movie)
-
-print(trainset)
-
-print(set2)
+print(imgs[0].split('\\')[0])
