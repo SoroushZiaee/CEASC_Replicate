da640447092e4d511bcea0cb3a3c4653f215a621
Elizaveta Yakubovskaya <eyakub@my.yorku.ca>
2025-04-14 02:03:47 -0400

    overfitting and metrics

--- scripts/metrics/accuracy.py
+++ scripts/metrics/accuracy.py
@@ -1,19 +1,29 @@
 # the accuracy (precision and recall) metrics we are focusing on reproducing
 import torch
 
+# first start by calculating IoU for a given set of 
+def iou():
+    ''' 
+    function to get the IoU for any two boxes 
+    '''
+
+
+# class-level AP and AR calculation
+def class_metrics():
+    '''
+    function to get the AP of any class at a threshold and AR of any class for to n predictions
+    '''
+
+# overall function to make it easy to do this across multiple classes and thresholds
+def main():
+    '''
+    computes AP50, AP75 and mAP across these two levels 
+    AR1, AR10, AR100, AR500
+    '''
+
 # mAP
 
-
 # AP50 and AP75 
-class AP(torch.nn.Module):
-    def __init__(self,t):
-        super(AP, self,t).__init__()
-        self.t = t # set the threshold
-    def forward():
-        """
-        computes the average precision 
-        """
-
 
 # AR1
 
